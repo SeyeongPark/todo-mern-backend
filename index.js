5f7e770371af19077f23e5bf5720dde204c6025a
SeyeongPark <lynn.yeong.102@gmail.com>
2022-07-23 12:53:07 -0400

    try to fix CORS policy error

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.use(express.json());
 app.use(cors());
 
 // routes
-app.use("/api/users", userRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/user", userOneRoutes);
+app.use("/api/users/", userRoutes);
+app.use("/api/auth/", authRoutes);
+app.use("/api/user/", userOneRoutes);
 
 require('dotenv').config();
 
